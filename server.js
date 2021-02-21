d2b0f0b82258597850989cf77132bf10ad7887b1
whatminjacodes <whatminjacodes@gmail.com>
2021-02-21 11:19:43 +0200

    Simple server file

---
+++ server.js
@@ -0,0 +1,14 @@
+var static = require('node-static');
+const https = require('https');
+const fs = require('fs');
+const options = {
+    // Needed for HTTPS certification
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
+const file = new (static.Server)();
+https.createServer(options, function (req, res) {
+    req.addListener('end', function () {
+        file.serve(req, res);
+    }).resume();
+}).listen(3000);
--- www/main.js
+++ www/main.js
@@ -1,6 +1,5 @@
 // button to start XR experience
 let xrButton = document.getElementById('xr-button');
-xrButton.innerHTML = 'Enter AR';
 
 // to control the xr session
 let xrSession = null;
@@ -10,3 +9,35 @@ let xrRefSpace = null;
 
 // Canvas OpenGL context used for rendering
 let gl = null;
+
+// Make sure WebXR is working as intended
+function checkXR() {
+    // Check if secure context is available
+    if (!window.isSecureContext) {
+      document.getElementById("warning").innerText = "Secure context needed.";
+    }
+    // Check if WebXR is supported
+    if (navigator.xr) {
+      navigator.xr.addEventListener('devicechange', checkXRSupportState);
+      checkXRSupportState();
+    } else {
+      document.getElementById("warning").innerText = "WebXR is not supported by this browser.";
+    }
+  }
+
+  // Check if browser supports WebXR and update the button
+  function checkXRSupportState() {
+    navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
+     updateXRButton(supported);
+    });
+  }
+
+  function updateXRButton(supported) {
+    if (supported) {
+        xrButton.innerHTML = 'Start AR';
+        xrButton.addEventListener('clicked', onButtonClicked);
+      } else {
+        xrButton.innerHTML = 'AR is not found';
+      }
+      xrButton.disabled = !supported;
+  }
