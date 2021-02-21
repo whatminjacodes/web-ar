f1c281b27086c0a0d78fa39dd3fa627c1f52cc35
whatminjacodes <whatminjacodes@gmail.com>
2021-02-21 13:33:04 +0200

    working arjs version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var static = require('node-static');
 const https = require('https');
 const fs = require('fs');
 const options = {
-    // Needed for HTTPS certification
+    // Needed for HTTPS certification, remember to generate before trying to run
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
 }
--- www/main.js
+++ www/main.js
@@ -11,7 +11,7 @@ let xrRefSpace = null;
 let gl = null;
 
 // Make sure WebXR is working as intended
-function checkXR() {
+function checkXRStatus() {
     // Check if secure context is available
     if (!window.isSecureContext) {
       document.getElementById("warning").innerText = "Secure context needed.";
@@ -32,12 +32,71 @@ function checkXR() {
     });
   }
 
+  // Update the XR start button depending on the browser status
   function updateXRButton(supported) {
     if (supported) {
         xrButton.innerHTML = 'Start AR';
-        xrButton.addEventListener('clicked', onButtonClicked);
+        xrButton.addEventListener('clicked', onXRButtonClicked);
       } else {
         xrButton.innerHTML = 'AR is not found';
       }
       xrButton.disabled = !supported;
   }
+
+  // XR button click listener
+  function onXRButtonClicked() {
+    if (!xrSession) {
+        navigator.xr.requestSession('immersive-ar', {
+            optionalFeatures: ['dom-overlay'],
+            requiredFeatures: ['local'],
+            domOverlay: {root: document.getElementById('overlay')}
+        }).then(onSessionStarted, onRequestSessionError);
+    } else {
+      xrSession.end();
+    }
+  }
+
+  // XR session logic
+  function onSessionStarted(session) {
+    xrSession = session;
+    xrButton.innerHTML = 'Exit AR';
+
+    session.addEventListener('end', onSessionEnded);
+
+    let canvas = document.createElement('canvas');
+    gl = canvas.getContext('webgl', { xrCompatible: true });
+    session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
+  
+    session.requestReferenceSpace('local').then((refSpace) => {
+      xrRefSpace = refSpace;
+      session.requestAnimationFrame(onXRFrame);
+    });
+  
+  }
+  
+  // Error on AR session
+  function onRequestSessionError(ex) {
+    document.getElementById('info').innerHTML = "AR session failed.";
+  }
+
+  // Session end logic
+  function onSessionEnded(event) {
+    xrSession = null;
+    xrButton.innerHTML = 'Start AR';
+    document.getElementById('info').innerHTML = '';
+    gl = null;
+  }
+
+  // Session loop
+  function onXRFrame(t, frame) {
+    let session = frame.session;
+    session.requestAnimationFrame(onXRFrame);
+    let pose = frame.getViewerPose(xrRefSpace);
+    if (!pose) {
+      return;
+    }
+    const pos = pose.transform.position;
+    info.innerHTML = `x:${pos.x.toFixed(2)} y:${pos.y.toFixed(2)} z:${pos.z.toFixed(2)}`;
+  }
+
+  checkXRStatus();
